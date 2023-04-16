7433f5b72b0c51ecc3f24691573973eceab34482
Muhamad Rizki Arif Fadillah <mrafkibo@gmail.com>
2023-04-16 21:41:17 +0700

    latihan coding JS dengan 10 soal baru dikerjakan 6

--- 9 object.js
+++
@@ -1,8 +0,0 @@
-let user =  {
-    nama : "Muhamad Rizki Arif Fadillah",
-    email : "kibo@gmail.com",
-    "tempat lahir" : "Purwakarta",
-}
-console.log(user);
-console.log(`Hallo perkenalkan nama saya ${user.nama}\nkalian bisa menghubungi saya via email: ${user.email}`);
-console.log(`saya lahir di ${user["tempat lahir"]}`)
